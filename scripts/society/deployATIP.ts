18b6b9df20beb7a7ad027a6e75effaf66660a773
kw <kahwai@pathdao.io>
2025-03-12 16:30:58 +0800

    updated deployment info

--- scripts/society/deployATIP.ts
+++ scripts/society/deployATIP.ts
@@ -3,12 +3,12 @@ import { ethers, upgrades } from "hardhat";
 (async () => {
   try {
     const args = require("../arguments/atipArguments");
-    const Contract = await ethers.getContractFactory("ATIPToken");
+    const Contract = await ethers.getContractFactory("ATIPSimple");
     const contract = await upgrades.deployProxy(Contract, args, {
       initialOwner: process.env.CONTRACT_CONTROLLER,
     });
     await contract.waitForDeployment();
-    console.log("ATIPToken deployed to:", contract.target);
+    console.log("ATIPSimple deployed to:", contract.target);
   } catch (e) {
     console.log(e);
   }

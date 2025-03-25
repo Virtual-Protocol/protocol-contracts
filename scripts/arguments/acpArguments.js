3ef7a91ff5a7c28b4c838500a93d798e29d025c3
kw <kahwai@pathdao.io>
2025-03-25 13:48:17 +0800

    updated deployment script

--- scripts/arguments/acpArguments.js
+++ scripts/arguments/acpArguments.js
@@ -1,7 +1,7 @@
 module.exports = [
-  process.env.PROVIDER_REGISTRY,
   process.env.REWARD_TOKEN,
   200, // 2%
   1, // 1 evaluator per job
-  1, // 1 signature
+  100, // platform fee 1%,
+  process.env.ACP_PLATFORM_TREASURY
 ];

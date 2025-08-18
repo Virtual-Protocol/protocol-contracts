04794ca4568c7a9d5b6f16a52c5c0444e24fbbb8
kw <kahwai@pathdao.io>
2025-08-18 23:05:11 +0800

    updated mainnet deployment info

--- scripts/arguments/agentETax.js
+++ scripts/arguments/agentETax.js
@@ -3,7 +3,7 @@ module.exports = [
   process.env.WETH,
   process.env.ETH_VIRTUAL,
   process.env.ETH_UNISWAP_ROUTER,
-  process.env.TAX_VAULT,
+  process.env.BUILDER_CODE_VAULT,
   process.env.MIN_SWAP_THRESHOLD,
   process.env.MAX_SWAP_THRESHOLD,
 ];
